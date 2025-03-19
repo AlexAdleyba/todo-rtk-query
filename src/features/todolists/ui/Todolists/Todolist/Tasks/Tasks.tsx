6833be80e70976292ebdcb8c383dc95e17002bcd
AlexAdleyba <light_lex@mail.ru>
2025-03-19 22:28:57 +0300

    update task pagination

--- src/features/todolists/ui/Todolists/Todolist/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/Todolist/Tasks/Tasks.tsx
@@ -1,42 +1,49 @@
 import List from "@mui/material/List"
-import { Task } from "./Task/Task"
-import { TaskStatus } from "../../../../lib/enums/enums"
-import { useGetTasksQuery } from "../../../../api/tasksApi"
-import { TasksSkeleton } from "../../../skeletons/TasksSkeleton/TasksSkeleton"
-import type { DomainTodolist } from "../../../../lib/types/types"
+import {Task} from "./Task/Task"
+import {TaskStatus} from "../../../../lib/enums/enums"
+import {PAGE_SIZE, useGetTasksQuery} from "../../../../api/tasksApi"
+import {TasksSkeleton} from "../../../skeletons/TasksSkeleton/TasksSkeleton"
+import type {DomainTodolist} from "../../../../lib/types/types"
+import {useState} from "react";
+import {TasksPagination} from "../TasksPagination/TasksPagination";
 
 type Props = {
-  todolist: DomainTodolist
+    todolist: DomainTodolist
 }
 
-export const Tasks = ({ todolist }: Props) => {
-  const { data, isLoading } = useGetTasksQuery(todolist.id)
+export const Tasks = ({todolist}: Props) => {
+    const [page, setPage] = useState(1)
+    const {data, isLoading} = useGetTasksQuery({todolistId: todolist.id, args: {page}})
 
-  let tasksForTodolist = data?.items
+    let tasksForTodolist = data?.items
 
-  if (todolist.filter === "active") {
-    tasksForTodolist = tasksForTodolist?.filter((task) => task.status === TaskStatus.New)
-  }
+    if (todolist.filter === "active") {
+        tasksForTodolist = tasksForTodolist?.filter((task) => task.status === TaskStatus.New)
+    }
 
-  if (todolist.filter === "completed") {
-    tasksForTodolist = tasksForTodolist?.filter((task) => task.status === TaskStatus.Completed)
-  }
+    if (todolist.filter === "completed") {
+        tasksForTodolist = tasksForTodolist?.filter((task) => task.status === TaskStatus.Completed)
+    }
 
-  if (isLoading) {
-    return <TasksSkeleton />
-  }
+    if (isLoading) {
+        return <TasksSkeleton/>
+    }
 
-  return (
-    <>
-      {tasksForTodolist?.length === 0 ? (
-        <p>Тасок нет</p>
-      ) : (
-        <List>
-          {tasksForTodolist?.map((task) => {
-            return <Task key={task.id} task={task} todolist={todolist} disabled={todolist.entityStatus === "loading"} />
-          })}
-        </List>
-      )}
-    </>
-  )
+    return (
+        <>
+            {tasksForTodolist?.length === 0 ? (
+                <p>Тасок нет</p>
+            ) : (
+                <>
+                    <List>
+                        {tasksForTodolist?.map((task) => {
+                            return <Task key={task.id} task={task} todolist={todolist}
+                                         disabled={todolist.entityStatus === "loading"}/>
+                        })}
+                    </List>
+                    {(data?.totalCount || 0) > PAGE_SIZE &&<TasksPagination totalCount={data?.totalCount || 0} page={page} setPage={setPage}/>}
+                </>
+            )}
+        </>
+    )
 }
